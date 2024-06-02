b2bef0cf0359c7717bfaa2a7d023e624ace87af8
raphdraft1 <90973519+raphdraft1@users.noreply.github.com>
2024-06-02 15:53:40 +0800

    Add files via upload

---
+++ Reversing a string.py
@@ -0,0 +1,8 @@
+## reversing a string
+
+def reverse_string(my_string):
+    reverse_string = my_string[::-1]
+    return reverse_string
+
+my_string = "That lemon pie is not for sale."
+result = Answer.reverse_string(my_string)
