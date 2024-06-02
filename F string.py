39fc1a9dcb67f7e8610861a880c1aaf291176553
raphdraft1 <90973519+raphdraft1@users.noreply.github.com>
2024-06-02 16:04:28 +0800

    Add files via upload

---
+++ F string.py
@@ -0,0 +1,8 @@
+## F string
+
+def calc_string(first_number, second_number):
+    return (f'The sum of {first_number} and {second_number} is {first_number + second_number}.')
+
+first_number = 5
+second_number = 9
+result = Answer.calc_string(first_number, second_number)
